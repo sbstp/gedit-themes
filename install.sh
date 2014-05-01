43e58eb57baecf722c95099edc76907b10ee73ec
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2014-05-01 15:13:30 -0400

    support old versions of gtk

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 
 mkdir -p ~/.local/share/gtksourceview-3.0/styles/
 cp styles/* ~/.local/share/gtksourceview-3.0/styles/
+mkdir -p ~/.local/share/gtksourceview-2.0/styles/
+cp styles/* ~/.local/share/gtksourceview-2.0/styles/
 echo "Done."
